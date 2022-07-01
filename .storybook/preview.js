4cc891c53b8afa84a614eb73e5bbb6e7b12a25f0
성진옥 <wlsdhr0831@naver.com>
2022-07-02 01:37:31 +0900

    feat: 라우팅 페이지에 스토리북 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { MemoryRouter } from "react-router-dom";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +8,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter initialEntries={["/"]}>
+      <Story />
+    </MemoryRouter>
+  ),
+];
---
+++ src/pages/Home/Home.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react";
+import Home from "./Home";
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+} as Meta;
+
+const Template = () => <Home />;
+
+export const 홈 = Template.bind({});
