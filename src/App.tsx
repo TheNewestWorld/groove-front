5d462b1b62e0cf580609081f87ea8608840facfa
성진옥 <wlsdhr0831@naver.com>
2022-07-02 05:25:18 +0900

    feat: navigation 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import RoutePath from "./constants/routePath";
 import Home from "./pages/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path={RoutePath.home} element={<Home />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path={RoutePath.home} element={<Home />} />
+      </Routes>
+      <Navigation />
+    </>
   );
 }
 
