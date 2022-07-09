e04c3db3f212023ce67f4d1e4a0e61e59ac3bee3
성진옥 <wlsdhr0831@naver.com>
2022-07-09 22:53:19 +0900

    feat: navigation css 수정 및 icon 적용

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,17 +1,66 @@
 import { useNavigate } from "react-router-dom";
+import {
+  CommunityActiveIcon,
+  CommunityInactiveIcon,
+  HomeActiveIcon,
+  HomeInactiveIcon,
+  MyPageActiveIcon,
+  MyPageInactiveIcon,
+  TrainingActiveIcon,
+  TrainingInactiveIcon,
+} from "../../assets/icon";
 import RoutePath from "../../constants/routePath";
 
 import "./Navigation.scss";
 
-const Navigation = () => {
+export interface props {
+  current: "HOME" | "COMMUNITY" | "TRAINING" | "MYPAGE";
+}
+
+const Navigation = ({ current }: props) => {
   const navigation = useNavigate();
 
   return (
     <div className="navigation">
-      <button onClick={() => navigation(RoutePath.home)}>홈</button>
-      <button onClick={() => navigation(RoutePath.community)}>커뮤니티</button>
-      <button onClick={() => navigation(RoutePath.training)}>트레이닝</button>
-      <button onClick={() => navigation(RoutePath.mypage)}>마이페이지</button>
+      <button
+        className={current === "HOME" ? "blue" : ""}
+        onClick={() => navigation(RoutePath.home)}
+      >
+        {current === "HOME" ? <HomeActiveIcon /> : <HomeInactiveIcon />}
+        <br />홈
+      </button>
+      <button
+        className={current === "COMMUNITY" ? "blue" : ""}
+        onClick={() => navigation(RoutePath.community)}
+      >
+        {current === "COMMUNITY" ? (
+          <CommunityActiveIcon />
+        ) : (
+          <CommunityInactiveIcon />
+        )}
+        <br />
+        커뮤니티
+      </button>
+      <button
+        className={current === "TRAINING" ? "blue" : ""}
+        onClick={() => navigation(RoutePath.training)}
+      >
+        {current === "TRAINING" ? (
+          <TrainingActiveIcon />
+        ) : (
+          <TrainingInactiveIcon />
+        )}
+        <br />
+        트레이닝
+      </button>
+      <button
+        className={current === "MYPAGE" ? "blue" : ""}
+        onClick={() => navigation(RoutePath.mypage)}
+      >
+        {current === "MYPAGE" ? <MyPageActiveIcon /> : <MyPageInactiveIcon />}
+        <br />
+        마이페이지
+      </button>
     </div>
   );
 };
