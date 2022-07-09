a4823de80a38ad83067547bb3b609f3e2e88dcd3
성진옥 <wlsdhr0831@naver.com>
2022-07-09 22:52:25 +0900

    feat: icon svg 추가

---
+++ src/assets/icon/index.tsx
@@ -0,0 +1,8 @@
+export { ReactComponent as HomeInactiveIcon } from "./home_inactive_icon.svg";
+export { ReactComponent as HomeActiveIcon } from "./home_active_icon.svg";
+export { ReactComponent as CommunityInactiveIcon } from "./community_inactive_icon.svg";
+export { ReactComponent as CommunityActiveIcon } from "./community_active_icon.svg";
+export { ReactComponent as MyPageInactiveIcon } from "./myPage_inactive_icon.svg";
+export { ReactComponent as MyPageActiveIcon } from "./myPage_active_icon.svg";
+export { ReactComponent as TrainingInactiveIcon } from "./training_inactive_icon.svg";
+export { ReactComponent as TrainingActiveIcon } from "./training_active_icon.svg";
