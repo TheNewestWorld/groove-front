814bd4b2bffbeeecca467ce5304b520cde4dd87e
Ho2eny <heoin122@gmail.com>
2022-07-30 19:40:13 +0900

    fix: 빠진 left, right onClick 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,18 +4,22 @@ interface props {
   title: string;
   left?: React.ReactNode;
   right?: React.ReactNode;
+  onLeftClick?: () => void;
+  onRightClick?: () => void;
 }
 
 const Header = ({
   title,
   left,
   right,
+  onLeftClick,
+  onRightClick
 }: props): React.ReactElement => {
   return (
     <header className='header'>
-      <div>{left}</div>
+      <div onClick={onLeftClick}>{left}</div>
       <div className='header__title'>{title}</div>
-      <div>{right}</div>
+      <div onClick={onRightClick}>{right}</div>
     </header>    
   );
 }
