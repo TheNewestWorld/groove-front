e3ccd826e3b29347a64e3361366c9e8b5686d6c7
성진옥 <wlsdhr0831@naver.com>
2022-08-14 09:56:43 +0900

    feat: 코칭내역 리스트 추가

---
+++ src/pages/TrainingHistory/TrainingHistory.view.stories.tsx
@@ -0,0 +1,53 @@
+import { Meta } from "@storybook/react";
+import TrainingHistoryView, { Props } from "./TrainingHistory.view";
+
+export default {
+  title: "Pages/TrainingHistory/views",
+  component: TrainingHistoryView,
+  args: {
+    activeFilter: "최신순",
+    filterList: ["최신순"],
+    onChangeFilter: (filter: string) => alert(filter),
+    trainingList: [
+      {
+        imageUrl: "",
+        nickname: "음치몸치박치",
+        date: new Date(),
+        content: "코칭 부탁드립니다.",
+        status: "ready",
+      },
+      {
+        imageUrl: "",
+        nickname: "음치몸치박치",
+        date: new Date(),
+        content: "코칭 부탁드립니다.",
+        status: "done",
+      },
+      {
+        imageUrl: "",
+        nickname: "음치몸치박치",
+        date: new Date(),
+        content: "코칭 부탁드립니다.",
+        status: "ready",
+      },
+      {
+        imageUrl: "",
+        nickname: "음치몸치박치",
+        date: new Date(),
+        content: "코칭 부탁드립니다.",
+        status: "done",
+      },
+      {
+        imageUrl: "",
+        nickname: "음치몸치박치",
+        date: new Date(),
+        content: "코칭 부탁드립니다.",
+        status: "done",
+      },
+    ],
+  },
+} as Meta;
+
+const Template = (args: Props) => <TrainingHistoryView {...args} />;
+
+export const 홈 = Template.bind({});
