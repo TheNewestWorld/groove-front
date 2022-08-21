68d0143d3cd7ffc1bfb3f9fc217c191770a04be5
성진옥 <wlsdhr0831@naver.com>
2022-08-21 22:39:17 +0900

    feat: 바텀시트 수정

--- src/components/BottomSheet/BottomSheet.stories.tsx
+++ src/components/BottomSheet/BottomSheet.stories.tsx
@@ -1,14 +1,30 @@
-import { Meta } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
 
 import BottomSheet, { Props } from "./BottomSheet.view";
 
 export default {
-  title: "components/CircleImage",
+  title: "components/BottomSheet",
   component: BottomSheet,
   args: {
+    title: "헤더 타이틀",
+    children: <>여기는 내용이다.</>,
+    hasCloseButton: true,
+    onClose: () => alert("닫기"),
   },
 } as Meta;
 
-const Template = (args: Props) => <BottomSheet {...args} />;
+const Template: Story<Props> = (args: Props) => <BottomSheet {...args} />;
 
-export const Default = Template.bind({});
+export const 닫기_있음 = Template.bind({});
+
+export const 닫기_없음 = Template.bind({});
+닫기_없음.args = {
+  hasCloseButton: false,
+};
+
+export const 헤더_있음 = Template.bind({});
+
+export const 헤더_없음 = Template.bind({});
+헤더_없음.args = {
+  title: "",
+};
--- src/components/BottomSheet/BottomSheet.view.tsx
+++ src/components/BottomSheet/BottomSheet.view.tsx
@@ -1,11 +1,43 @@
+import { useRef } from "react";
+import { CloseIcon } from "../../assets/icon";
 import styles from "./BottomSheet.module.scss";
 
 export interface Props {
-
+  title?: string;
+  hasCloseButton?: boolean;
+  children?: React.ReactNode;
+  onClose: () => void;
 }
 
-const BottomSheetView = ({}: Props) => {
-    return <></>
+const BottomSheetView = ({
+  title,
+  hasCloseButton = false,
+  children,
+  onClose,
+}: Props) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      ref={containerRef}
+      className={styles.container}
+      onClick={(e) => {
+        if (e.target === containerRef.current) {
+          onClose();
+        }
+      }}
+    >
+      <div className={styles.white}>
+        <div className={styles.header}>
+          {title && <span>{title}</span>}
+          {hasCloseButton && (
+            <CloseIcon className={styles.icon} onClick={onClose} />
+          )}
+        </div>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default BottomSheetView;
