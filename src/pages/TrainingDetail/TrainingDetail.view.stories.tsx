756f1ca9c26e866e81d433ce2a593c9803c9fb7a
성진옥 <wlsdhr0831@naver.com>
2022-08-21 20:32:58 +0900

    feat: 화면 케이스 추가

--- src/pages/TrainingDetail/TrainingDetail.view.stories.tsx
+++ src/pages/TrainingDetail/TrainingDetail.view.stories.tsx
@@ -1,12 +1,73 @@
-import { Meta } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
 import TrainingDetailView, { Props } from "./TrainingDetail.view";
 
 export default {
   title: "Pages/TrainingDetail/views",
   component: TrainingDetailView,
-  args: {},
+  args: {
+    title: "정승환이 되고 싶습니다.",
+    nickname: "승환정",
+    profileImage: "",
+    date: new Date(),
+    content: "가나다라마바사아자차카타파하",
+    role: "writer",
+    status: "ready",
+    feedback: {
+      title: "정승환이 되고 싶으세요?",
+      nickname: "트레이너",
+      profileImage: "",
+      date: new Date(),
+      content: "가나다라마바사아자차카타파하",
+    },
+  },
 } as Meta;
 
-const Template = (args: Props) => <TrainingDetailView {...args} />;
+const Template: Story<Props> = (args: Props) => (
+  <TrainingDetailView {...args} />
+);
 
-export const 기본 = Template.bind({});
+export const 작성자_피드백_있음 = Template.bind({});
+작성자_피드백_있음.args = {
+  status: "in-progress",
+};
+
+export const 트레이너_피드백_있음 = Template.bind({});
+트레이너_피드백_있음.args = {
+  role: "trainer",
+  status: "in-progress",
+};
+
+export const 기타_피드백_있음 = Template.bind({});
+기타_피드백_있음.args = {
+  role: "unknown",
+};
+
+export const 작성자_피드백_없음 = Template.bind({});
+작성자_피드백_없음.args = {
+  status: "ready",
+  feedback: undefined,
+};
+
+export const 트레이너_피드백_없음 = Template.bind({});
+트레이너_피드백_없음.args = {
+  feedback: undefined,
+  role: "trainer",
+  status: "ready",
+};
+
+export const 기타_피드백_없음 = Template.bind({});
+기타_피드백_없음.args = {
+  feedback: undefined,
+  role: "unknown",
+};
+
+export const 트레이너_코칭_완료 = Template.bind({});
+트레이너_코칭_완료.args = {
+  role: "trainer",
+  status: "done",
+};
+
+export const 작성자_코칭_완료 = Template.bind({});
+작성자_코칭_완료.args = {
+  status: "done",
+};
