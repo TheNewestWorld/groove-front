bb1a93778c9dc40d1a055eb3d8e4f1700d53f2b8
성진옥 <wlsdhr0831@naver.com>
2022-08-21 16:47:19 +0900

    feat: 외부 클릭 시 바텀시트 꺼짐

--- src/components/BottomSheetList/BottomSheetList.tsx
+++ src/components/BottomSheetList/BottomSheetList.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useRef } from "react";
 import { CloseIcon } from "../../assets/icon";
 import "./BottomSheetList.scss";
 
@@ -7,7 +8,8 @@ export interface Props {
   list: string[];
   activeItem?: string;
   onClick?: (value: string) => void;
-  onClose?: () => void;
+  hasCloseButton?: boolean;
+  onClose: () => void;
 }
 
 const BottomSheetList = ({
@@ -15,31 +17,44 @@ const BottomSheetList = ({
   list,
   activeItem = "",
   onClick,
+  hasCloseButton = true,
   onClose,
 }: Props) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div className="bottom-sheet-list">
-      <div className="bottom-sheet-list__header">
-        {header && <span>{header}</span>}
-        {onClose && (
-          <CloseIcon className="bottom-sheet-list__icon" onClick={onClose} />
-        )}
-      </div>
-      {list.map((item) => (
-        <div
-          key={item}
-          className={classNames([
-            "bottom-sheet-list__item",
-            activeItem === item && "active",
-          ])}
-          onClick={() => {
-            onClick?.(item);
-            onClose?.();
-          }}
-        >
-          {item}
+    <div
+      ref={containerRef}
+      className="bottom-sheet-list__container"
+      onClick={(e) => {
+        if (e.target === containerRef.current) {
+          onClose();
+        }
+      }}
+    >
+      <div className="bottom-sheet-list">
+        <div className="bottom-sheet-list__header">
+          {header && <span>{header}</span>}
+          {hasCloseButton && (
+            <CloseIcon className="bottom-sheet-list__icon" onClick={onClose} />
+          )}
         </div>
-      ))}
+        {list.map((item) => (
+          <div
+            key={item}
+            className={classNames([
+              "bottom-sheet-list__item",
+              activeItem === item && "active",
+            ])}
+            onClick={() => {
+              onClick?.(item);
+              onClose?.();
+            }}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
