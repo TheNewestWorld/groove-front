e8a6a5d018e7542c5b8118c9349a6651cb41dd3c
devhwi <gkgkgldy@gmail.com>
2022-08-27 16:43:52 +0900

    feat: add empty page

---
+++ src/components/EmptyPage/EmptyPage.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+
+import styles from "./EmptyPage.module.scss";
+
+export interface Props {
+  title?: string;
+  description?:string;
+  className?:string
+}
+
+const EmptyPage = ({
+  title,
+  description,
+  className,
+}: Props): React.ReactElement => {
+  return (
+    <div className={classNames([className, styles.container])}>
+      <div className={styles.message}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyPage;
