4686ebca64be780c6364cf5c3e3a46d6b97d1e13
gnlwhd6498 <gkgkgldy@gmail.com>
2022-09-22 21:33:04 +0900

    modify select category

--- src/components/SelectCategory/SelectCategory.stories.tsx
+++ src/components/SelectCategory/SelectCategory.stories.tsx
@@ -1,14 +1,16 @@
 import { Meta, Story } from "@storybook/react";
 import SelectCategory, { Props } from "./SelectCategory";
 
+
 export default {
   title: 'components/SelectCategory',
   component: SelectCategory,
   args: {
     hasCloseButton: false,
     title: "카테고리를 선택해주세요.",
-    activeCategory: "카테고리를 선택해주세요.",
-    categoryList: ["보컬 이야기", "일상 이야기", "기타"]
+    activeCategory: "카테고리를 선택해주세요..",
+    categoryList: ["보컬 이야기", "일상 이야기", "기타"],
+    onClickCategory: (category: string) => alert(`${category}`),
   },
 } as Meta;
 
--- src/components/SelectCategory/SelectCategory.tsx
+++ src/components/SelectCategory/SelectCategory.tsx
@@ -3,23 +3,25 @@ import BottomSheetList from "../BottomSheetList";
 import { SelectIcon } from "../../assets/icon";
 import useOpen from "../../hooks/useOpen";
 import styles from "./SelectCategory.module.scss";
-import useCategory from "../../hooks/selectCategory";
 
 export interface Props {
   title?: string;
   className?: string;
+  activeCategory: string
   categoryList: string[];
   hasCloseButton?: boolean;
+  onClickCategory: (category: string) => string;
 }
 
 const SelectCategory = ({
+  title,
   className,
+  activeCategory,
+  onClickCategory,
   categoryList,
   hasCloseButton,
-  title,
 }: Props) => {
   const { isOpen, onOpen, onClose } = useOpen();
-  const { activeCategory, clickCategory } = useCategory();
   return (
     <div>
       <div className={classNames([className, styles.container, styles.category, styles.category__active])} onClick={onOpen}>
@@ -32,7 +34,7 @@ const SelectCategory = ({
           hasCloseButton={hasCloseButton}
           list={categoryList}
           activeItem={activeCategory}
-          onClick={clickCategory}
+          onClick={onClickCategory}
           onClose={onClose}
         />
       )}
