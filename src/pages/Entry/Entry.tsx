4a2fb7de8da702ca55a1ea352f22361103980241
성진옥 <wlsdhr0831@naver.com>
2022-09-27 22:16:26 +0900

    Feat: Entry 페이지 추가

---
+++ src/pages/Entry/Entry.tsx
@@ -0,0 +1,10 @@
+import EntryView from "./Entry.view";
+
+const Entry = () => {
+  const goToSignIn = () => {};
+  const goToSignUp = () => {};
+
+  return <EntryView goToSignIn={goToSignIn} goToSignUp={goToSignUp} />;
+};
+
+export default Entry;
