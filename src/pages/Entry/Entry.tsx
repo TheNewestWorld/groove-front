b58339689fad234b67e2f1c9bc3c23b9646269e9
성진옥 <wlsdhr0831@naver.com>
2022-09-27 22:17:08 +0900

    chore: TODO 작성

--- src/pages/Entry/Entry.tsx
+++ src/pages/Entry/Entry.tsx
@@ -1,7 +1,9 @@
 import EntryView from "./Entry.view";
 
 const Entry = () => {
+  // TODO
   const goToSignIn = () => {};
+  // TODO
   const goToSignUp = () => {};
 
   return <EntryView goToSignIn={goToSignIn} goToSignUp={goToSignUp} />;
