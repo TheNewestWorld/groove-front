e7cc07b7b7088031dc9b000662e88597004c6ebb
Ho2eny <heoin122@gmail.com>
2022-10-03 22:24:05 +0900

    feat: CommunityComment 페이지 추가

---
+++ src/pages/CommunityComment/CommunityComment.tsx
@@ -0,0 +1,5 @@
+const CommunityComment = () => {
+  return <></>;
+};
+
+export default CommunityComment;
---
+++ src/pages/CommunityComment/CommunityComment.view.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+import Header from "../../components/Header";
+import styles from "./CommunityComment.module.scss";
+
+import CommunityCommentItem, {
+  Props as CommunityCommentItemType,
+} from "./components/CommunityCommentItem";
+
+import { CloseIcon } from "../../assets/icon";
+import CommunityCommentFooter from "./components/CommunityCommentFooter";
+import CommunityCommentReadMore from "./components/CommunityCommentReadMore";
+
+interface CommentReadMore {
+  isOpen: boolean;
+  limit: number;
+}
+
+export interface CommentPair {
+  comment: CommunityCommentItemType;
+  replies: CommunityCommentItemType[];
+}
+
+export interface Props {
+  comments?: CommentPair[];
+  onCloseClick: () => void;
+  onSubmitComment: (comment: string) => void;
+}
+
+const CommunityComment = ({
+  comments = [],
+  onCloseClick,
+  onSubmitComment,
+}: Props) => {
+  const [commentsReadMore, setCommentsReadMore] = useState<CommentReadMore[]>(
+    new Array(comments.length).fill({
+      isOpen: true,
+      limit: 10,
+    }),
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerBody}>
+        <Header
+          title="댓글"
+          right={<CloseIcon />}
+          onRightClick={onCloseClick}
+        />
+        {comments.map((item, index) => (
+          <div>
+            <CommunityCommentItem {...item.comment} />
+
+            {item.replies &&
+              item.replies.length > 0 &&
+              commentsReadMore[index].isOpen &&
+              item.replies.map(reply => (
+                <CommunityCommentItem {...reply} className={styles.reply} />
+              ))}
+
+            {item.replies && item.replies.length > 0 && (
+              <CommunityCommentReadMore
+                className={styles.readMore}
+                isOpen={commentsReadMore[index].isOpen}
+                replyCount={item.replies.length}
+                onClick={() => {
+                  setCommentsReadMore(
+                    commentsReadMore.map((item, i) => {
+                      if (index == i) {
+                        return {
+                          ...item,
+                          ["isOpen"]: !commentsReadMore[index].isOpen,
+                        };
+                      } else {
+                        return item;
+                      }
+                    }),
+                  );
+                }}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+      <CommunityCommentFooter onSubmitComment={onSubmitComment} />
+    </div>
+  );
+};
+
+export default CommunityComment;
