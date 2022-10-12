3ceb6a07bc078edd9499200397dac4db6e909e41
gnlwhd6498 <gkgkgldy@gmail.com>
2022-10-12 22:35:11 +0900

    feat: add tab list

--- src/pages/CommunityList/CommunityList.view.tsx
+++ src/pages/CommunityList/CommunityList.view.tsx
@@ -2,8 +2,8 @@ import CommunityItem, {
   Props as CommunityItemView,
 } from "../../components/CommunityItem";
 import TopDownFilter from "../../components/TopDownFilter";
-
-import "./CommunityList.scss";
+import styles from "./CommunityList.module.scss";
+import TabList from "../../components/TabList";
 
 export interface Props {
   activeFilter: string;
@@ -19,15 +19,21 @@ const CommunityListView = ({
   communityList,
 }: Props) => {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <TabList
+        type="round"
+        activeTab="전체 게시물"
+        tabList={["전체 게시물", "보컬 이야기", "일상 이야기", "기타"]}
+        onClickTab={(tab: string) => alert(tab)}
+      />
       <TopDownFilter
-        className="filter"
+        className={styles.filter}
         activeFilter={activeFilter}
         filterList={filterList}
         onClickFilter={onChangeFilter}
       />
       {communityList.map((item, index) => (
-        <div className="item">
+        <div className={styles.item}>
           <CommunityItem key={index} {...item} />
         </div>
       ))}
