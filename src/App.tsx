c0bf698f3e0963cc22f85b06d5116a6c36f1fa3f
성진옥 <wlsdhr0831@naver.com>
2022-10-16 03:17:12 +0900

    feat: 에러 바운더리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,30 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import FindPassword from "./pages/FindPassword";
 import ResetPassword from "./pages/ResetPassword";
+import {
+  GlobalApiErrorBoundary,
+  GlobalErrorBoundary,
+} from "./common/errors/boundaries";
+import { QueryClientProvider } from "react-query";
+import { getGlobalQueryClient } from "./common/configs";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path={RoutePath.entry} element={<Entry />} />
-        <Route path={RoutePath.signIn} element={<SignIn />} />
-        <Route path={RoutePath.signUp} element={<SignUp />} />
-        <Route path={RoutePath.findPassword} element={<FindPassword />} />
-        <Route path={RoutePath.resetPassword} element={<ResetPassword />} />
-        <Route path={RoutePath.home} element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <GlobalErrorBoundary>
+      <QueryClientProvider client={getGlobalQueryClient()}>
+        <GlobalApiErrorBoundary>
+          <Routes>
+            <Route path={RoutePath.entry} element={<Entry />} />
+            <Route path={RoutePath.signIn} element={<SignIn />} />
+            <Route path={RoutePath.signUp} element={<SignUp />} />
+            <Route path={RoutePath.findPassword} element={<FindPassword />} />
+            <Route path={RoutePath.resetPassword} element={<ResetPassword />} />
+            <Route path={RoutePath.home} element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </GlobalApiErrorBoundary>
+      </QueryClientProvider>
+    </GlobalErrorBoundary>
   );
 }
 
