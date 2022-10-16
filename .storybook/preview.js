efb51eaac8016f6cc9b17528b722fb3a5c7984c9
성진옥 <wlsdhr0831@naver.com>
2022-10-17 00:18:48 +0900

    chore: 사용하지 않는 라이브러리 import 삭제

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
 import { addDecorator } from "@storybook/react";
 import { initializeWorker, mswDecorator } from "msw-storybook-addon";
-import { MemoryRouter } from "react-router-dom";
 
 initializeWorker();
 addDecorator(mswDecorator);
