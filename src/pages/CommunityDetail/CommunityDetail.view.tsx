52430e843151a39d55c93a749a6b2e27d86f3d01
Ho2eny <heoin122@gmail.com>
2022-10-16 20:35:34 +0900

    fix: Presentation Props를 노출시키지 않도록 수정

--- src/pages/CommunityDetail/CommunityDetail.view.tsx
+++ src/pages/CommunityDetail/CommunityDetail.view.tsx
@@ -3,29 +3,42 @@ import Header from "../../components/Header";
 
 import { ArrowTailIcon, ReportIcon } from "../../assets/icon";
 
-import ContentHeader, {
-  Props as ContentHeaderType,
-} from "../../components/ContentHeader";
-
-import ContentBody, {
-  Props as ContentBodyType,
-} from "../../components/ContentBody";
-import CommunityFooter, {
-  Props as CommunityFooterType,
-} from "../../components/CommunityFooter";
+import ContentHeader from "../../components/ContentHeader";
+import ContentBody from "../../components/ContentBody";
+import CommunityFooter from "../../components/CommunityFooter";
 
 export interface Props {
-  contentHeader: ContentHeaderType;
-  contentBody: ContentBodyType;
-  footer: CommunityFooterType;
+  title: string;
+  profileImage: string;
+  nickname: string;
+  date: Date;
+  onClickProfile?: () => void;
+  content: string;
+  imageList?: { src: string; id: number }[];
+  audio: { title: string; src: string; id: number };
+  onClickImage?: (id: number) => void;
+  onClickMore?: () => void;
+  likeCount: number;
+  liked?: boolean;
+  commentCount: number;
   onBackClick?: () => void;
   onOptionClick?: () => void;
 }
 
 const CommunityDetailView = ({
-  contentHeader,
-  contentBody,
-  footer,
+  title,
+  profileImage,
+  nickname,
+  date,
+  onClickProfile,
+  content,
+  imageList,
+  audio,
+  onClickImage,
+  onClickMore,
+  likeCount,
+  liked,
+  commentCount,
   onBackClick,
   onOptionClick,
 }: Props) => {
@@ -38,9 +51,27 @@ const CommunityDetailView = ({
         onLeftClick={onBackClick}
         onRightClick={onOptionClick}
       />
-      <ContentHeader className={styles.header} {...contentHeader} />
-      <ContentBody className={styles.content} {...contentBody} />
-      <CommunityFooter {...footer} />
+      <ContentHeader
+        className={styles.header}
+        title={title}
+        profileImage={profileImage}
+        nickname={nickname}
+        date={date}
+        onClickProfile={onClickProfile}
+      />
+      <ContentBody
+        className={styles.content}
+        content={content}
+        imageList={imageList}
+        audio={audio}
+        onClickImage={onClickImage}
+        onClickMore={onClickMore}
+      />
+      <CommunityFooter
+        likeCount={likeCount}
+        liked={liked}
+        commentCount={commentCount}
+      />
     </div>
   );
 };
