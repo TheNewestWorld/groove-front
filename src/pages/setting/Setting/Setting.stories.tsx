0be054b2b0e2a02d05d37da030bf7cfec0d05139
성진옥 <wlsdhr0831@naver.com>
2022-11-04 05:32:31 +0900

    feat: 설정 화면 추가

---
+++ src/pages/setting/Setting/Setting.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta } from "@storybook/react";
+import Setting from "./Setting";
+
+export default {
+  title: "Pages/setting/Setting/views",
+  component: Setting,
+  args: {},
+} as Meta;
+
+const Template = () => <Setting />;
+
+export const 설정 = Template.bind({});
