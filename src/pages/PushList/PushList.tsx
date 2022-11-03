a124ad0a5aa086b309f7c376cf2550c11b4cab6f
gnlwhd6498 <gkgkgldy@gmail.com>
2022-11-03 21:38:37 +0900

    feat: add push list

---
+++ src/pages/PushList/PushList.tsx
@@ -0,0 +1,5 @@
+const PushList = () => {
+  return <></>;
+};
+
+export default PushList;
---
+++ src/pages/PushList/PushList.view.tsx
@@ -0,0 +1,76 @@
+import styles from "./PushList.module.scss";
+import { ArrowIcon } from "../../assets/icon";
+import Header from "../../components/Header";
+import CircleImage from "../../components/CircleImage";
+import EmptyPage from "../../components/EmptyPage";
+
+export interface Props {
+  onClickBack?: () => void;
+  monthList: { src: string; content: string; ago: string; }[];
+  pastList: { src: string; content: string; ago: string; }[];
+  emptyText: string;
+  src?: string;
+}
+
+const PushList = ({
+  onClickBack,
+  monthList,
+  pastList,
+  emptyText,
+  src,
+}: Props) => {
+  const hasMonthList = monthList.length > 0;
+  const hasPastList = pastList.length > 0;
+  return (
+    <div>
+      <Header
+        title="알림"
+        left={<ArrowIcon />}
+        onClickLeft={onClickBack}
+      />
+      {hasMonthList &&
+        <div className={styles.title}>이번 달</div>
+      }
+
+      {hasMonthList &&
+        monthList.map((item, index) => (
+          <div className={styles.items} key={index}>
+            <CircleImage className={styles.image} src={item.src} />
+            <div>
+              <div className={styles.content}>{item.content}</div>
+              <div className={styles.ago}>{item.ago}</div>
+            </div>
+          </div>
+        ))
+      }
+      {(hasMonthList && hasPastList) &&
+        <div className={styles.division} />
+      }
+
+      {hasPastList &&
+        <div className={styles.title}>이전 활동</div>
+      }
+
+      {hasPastList &&
+        pastList.map((item, index) => (
+          <div className={styles.items} key={index}>
+            <CircleImage className={styles.image} src={item.src} />
+            <div>
+              <div className={styles.content}>{item.content}</div>
+              <div className={styles.ago}>{item.ago}</div>
+            </div>
+          </div>
+        ))
+      }
+
+      {(!hasMonthList && !hasPastList) &&
+        <div>
+          <img className={styles.emptyImage} src={src} alt="" />
+          <EmptyPage className={styles.empty} description={emptyText} />
+        </div>
+      }
+    </div>
+  );
+};
+
+export default PushList;
