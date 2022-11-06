5553d43a9219fe074e73903aecbd3856a520f2a2
성진옥 <wlsdhr0831@naver.com>
2022-11-06 19:12:40 +0900

    feat: ListSection 적용 및 네이밍 수정

--- src/pages/PushList/PushList.view.tsx
+++ src/pages/PushList/PushList.view.tsx
@@ -1,48 +1,30 @@
 import styles from "./PushList.module.scss";
 import { ArrowIcon } from "../../assets/icon";
 import Header from "../../components/Header";
-import CircleImage from "../../components/CircleImage";
+import ListSection from "./components/ListSection";
 
 export interface Props {
+  currentMonthList?: { src: string; content: string; ago: string }[];
+  pastMonthList?: { src: string; content: string; ago: string }[];
   onClickBack?: () => void;
-  monthList: { src: string; content: string; ago: string }[];
-  pastList: { src: string; content: string; ago: string }[];
-  src?: string;
 }
 
-const PushList = ({ onClickBack, monthList, pastList, src }: Props) => {
-  const hasMonthList = monthList.length > 0;
-  const hasPastList = pastList.length > 0;
+const PushList = ({ onClickBack, currentMonthList, pastMonthList }: Props) => {
+  const showDivider = currentMonthList && pastMonthList;
 
   return (
     <div>
       <Header title="알림" left={<ArrowIcon />} onClickLeft={onClickBack} />
-      {hasMonthList && <div className={styles.title}>이번 달</div>}
 
-      {hasMonthList &&
-        monthList.map((item, index) => (
-          <div className={styles.items} key={index}>
-            <CircleImage className={styles.image} src={item.src} />
-            <div>
-              <div className={styles.content}>{item.content}</div>
-              <div className={styles.ago}>{item.ago}</div>
-            </div>
-          </div>
-        ))}
-      {hasMonthList && hasPastList && <div className={styles.division} />}
+      {currentMonthList && (
+        <ListSection sectionTitle="이번 달" list={currentMonthList} />
+      )}
 
-      {hasPastList && <div className={styles.title}>이전 활동</div>}
+      {showDivider && <div className={styles.division} />}
 
-      {hasPastList &&
-        pastList.map((item, index) => (
-          <div className={styles.items} key={index}>
-            <CircleImage className={styles.image} src={item.src} />
-            <div>
-              <div className={styles.content}>{item.content}</div>
-              <div className={styles.ago}>{item.ago}</div>
-            </div>
-          </div>
-        ))}
+      {pastMonthList && (
+        <ListSection sectionTitle="이전 활동" list={pastMonthList} />
+      )}
     </div>
   );
 };
