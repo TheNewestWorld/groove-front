87e333ca95b3b10e94eb60df975f05f8e808f746
성진옥 <wlsdhr0831@naver.com>
2022-11-06 19:04:31 +0900

    feat: EmptyState 삭제

--- src/pages/PushList/PushList.view.tsx
+++ src/pages/PushList/PushList.view.tsx
@@ -2,35 +2,22 @@ import styles from "./PushList.module.scss";
 import { ArrowIcon } from "../../assets/icon";
 import Header from "../../components/Header";
 import CircleImage from "../../components/CircleImage";
-import EmptyPage from "../../components/EmptyPage";
 
 export interface Props {
   onClickBack?: () => void;
-  monthList: { src: string; content: string; ago: string; }[];
-  pastList: { src: string; content: string; ago: string; }[];
-  emptyText: string;
+  monthList: { src: string; content: string; ago: string }[];
+  pastList: { src: string; content: string; ago: string }[];
   src?: string;
 }
 
-const PushList = ({
-  onClickBack,
-  monthList,
-  pastList,
-  emptyText,
-  src,
-}: Props) => {
+const PushList = ({ onClickBack, monthList, pastList, src }: Props) => {
   const hasMonthList = monthList.length > 0;
   const hasPastList = pastList.length > 0;
+
   return (
     <div>
-      <Header
-        title="알림"
-        left={<ArrowIcon />}
-        onClickLeft={onClickBack}
-      />
-      {hasMonthList &&
-        <div className={styles.title}>이번 달</div>
-      }
+      <Header title="알림" left={<ArrowIcon />} onClickLeft={onClickBack} />
+      {hasMonthList && <div className={styles.title}>이번 달</div>}
 
       {hasMonthList &&
         monthList.map((item, index) => (
@@ -41,15 +28,10 @@ const PushList = ({
               <div className={styles.ago}>{item.ago}</div>
             </div>
           </div>
-        ))
-      }
-      {(hasMonthList && hasPastList) &&
-        <div className={styles.division} />
-      }
+        ))}
+      {hasMonthList && hasPastList && <div className={styles.division} />}
 
-      {hasPastList &&
-        <div className={styles.title}>이전 활동</div>
-      }
+      {hasPastList && <div className={styles.title}>이전 활동</div>}
 
       {hasPastList &&
         pastList.map((item, index) => (
@@ -60,15 +42,7 @@ const PushList = ({
               <div className={styles.ago}>{item.ago}</div>
             </div>
           </div>
-        ))
-      }
-
-      {(!hasMonthList && !hasPastList) &&
-        <div>
-          <img className={styles.emptyImage} src={src} alt="" />
-          <EmptyPage className={styles.empty} description={emptyText} />
-        </div>
-      }
+        ))}
     </div>
   );
 };
