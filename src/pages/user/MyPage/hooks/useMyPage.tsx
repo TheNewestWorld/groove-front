f2eab5ca53852dada2d7b52bd863beeb89c0b891
성진옥 <wlsdhr0831@naver.com>
2022-11-27 02:12:43 +0900

    feat: 마이페이지에 query 추가

--- src/pages/user/MyPage/hooks/useMyPage.tsx
+++ src/pages/user/MyPage/hooks/useMyPage.tsx
@@ -1,5 +1,6 @@
 import useLikedPostListQuery from "../../../../common/queries/myPost/useLikedPostListQuery";
 import useWrittenPostListQuery from "../../../../common/queries/myPost/useWrittenPostListQuery";
+import useUserInfoQuery from "../../../../common/queries/users/useUserInfoQuery";
 
 const useMyPage = () => {
   const {
@@ -14,10 +15,16 @@ const useMyPage = () => {
     likedList,
   } = useLikedPostListQuery({});
 
+  const {
+    isLoading: isLoadingUserInfo,
+    isError: isErrorUserInfo,
+    data: userInfo,
+  } = useUserInfoQuery();
+
   return {
-    isLoading: isLoadingWrittenPost || isLoadingLikedPost,
-    isError: isErrorWrittenPost || isErrorLikedPost,
-    profileImage: "", // TODO
+    isLoading: isLoadingWrittenPost || isLoadingLikedPost || isLoadingUserInfo,
+    isError: isErrorWrittenPost || isErrorLikedPost || isErrorUserInfo,
+    profileImage: userInfo?.profileUrl ?? "",
     nickname: "", // TODO
     recordList: [], // TODO
     likedList,
