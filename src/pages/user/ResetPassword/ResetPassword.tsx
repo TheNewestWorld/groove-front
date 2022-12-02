46f6683ecdd4ef97adf01ad795ce108c0ef6da41
성진옥 <wlsdhr0831@naver.com>
2022-12-02 23:48:21 +0900

    feat: path 내의 query에서 값 추출

--- src/pages/user/ResetPassword/ResetPassword.tsx
+++ src/pages/user/ResetPassword/ResetPassword.tsx
@@ -1,6 +1,13 @@
+import { useSearchParams } from "react-router-dom";
 import ResetPasswordView from "./ResetPassword.view";
 
 const ResetPassword = () => {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("sessionKey");
+
+  // 디코딩하기
+  console.log(query);
+
   return <ResetPasswordView />;
 };
 
