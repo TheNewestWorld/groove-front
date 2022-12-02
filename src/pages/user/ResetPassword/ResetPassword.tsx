7c243c8ef1418874d5c1ed9e3e30cf9c1c63a34a
성진옥 <wlsdhr0831@naver.com>
2022-12-03 00:23:00 +0900

    feat: 비밀번호 변경 API 적용

--- src/pages/user/ResetPassword/ResetPassword.tsx
+++ src/pages/user/ResetPassword/ResetPassword.tsx
@@ -1,14 +1,26 @@
 import { useSearchParams } from "react-router-dom";
+import { setPassword } from "../../../common/apis/users/setPassword";
 import ResetPasswordView from "./ResetPassword.view";
 
 const ResetPassword = () => {
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("sessionKey");
+  const sessionKey = searchParams.get("sessionKey");
 
   // 디코딩하기
-  console.log(query);
+  console.log(sessionKey);
 
-  return <ResetPasswordView />;
+  if (!sessionKey) {
+    // TODO: 에러 표시
+    return <></>;
+  }
+
+  const updatePassword = (password: string) => {
+    setPassword({ sessionKey, password })
+      .then(() => {})
+      .catch(() => {});
+  };
+
+  return <ResetPasswordView onSubmit={updatePassword} />;
 };
 
 export default ResetPassword;
--- src/pages/user/ResetPassword/ResetPassword.view.tsx
+++ src/pages/user/ResetPassword/ResetPassword.view.tsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 import Form from "../../../components/Form";
 import Input from "../../../components/Input";
 import styles from "./ResetPassword.module.scss";
 
-export interface Props {}
+export interface Props {
+  onSubmit: (password: string) => void;
+}
 
-const ResetPasswordView = () => {
+const ResetPasswordView = ({ onSubmit }: Props) => {
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const checkPassword = () => {
+    if (password !== confirmPassword) {
+      return;
+    }
+
+    onSubmit(password);
+  };
+
   return (
     <Form
       title="비밀번호 재설정하기"
       subTitle="새로운 비밀번호를 입력해주세요."
       buttonText="완료하기"
       buttonColorTheme="dark"
+      onClick={checkPassword}
     >
       <div className={styles.inputGroup}>
         <Input
           label="비밀번호"
           placeholder="영문, 숫자, 특수문자 조합 8자리 이상"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <Input
           label="비밀번호 확인"
           placeholder="비밀번호를 다시 입력해주세요."
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
       </div>
     </Form>
