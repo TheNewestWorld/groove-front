f9ea857ae5aefce38623116e1df97a07f2ef9af5
성진옥 <wlsdhr0831@naver.com>
2022-12-03 14:05:42 +0900

    feat: 서버에서 내려오는 에러 메세지 노출

--- src/common/configs/axios/helpers.ts
+++ src/common/configs/axios/helpers.ts
@@ -11,12 +11,8 @@ export const resultData = async <T = any>(
 ): Promise<T> => {
   try {
     const result = await promiseObject;
-
     return result.data.data;
   } catch (error) {
-    // TODO: 실제 에러가 발생했을 때 객체 데이터 어떻게 감지 되는지 확인 후
-    // result.data.error 뽑아내기
-    console.log(error);
     throw error;
   }
 };
