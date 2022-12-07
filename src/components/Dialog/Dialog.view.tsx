fb4b9acc37662b2830e29e1045eb957410683108
성진옥 <wlsdhr0831@naver.com>
2022-12-08 02:57:42 +0900

    feat: 다이얼로그 아이콘 추가

--- src/components/Dialog/Dialog.view.tsx
+++ src/components/Dialog/Dialog.view.tsx
@@ -3,15 +3,19 @@ import "./Dialog.scss";
 
 export interface Props {
   title?: string;
+  icon?: React.ReactNode;
   children?: React.ReactNode;
   className?: string;
 }
 
-const DialogView = ({ title, children, className }: Props) => {
+const DialogView = ({ title, icon, children, className }: Props) => {
   return (
     <div className={classNames(["dialog__background", className])}>
       <div className="dialog__container">
-        <div className="dialog__title">{title}</div>
+        <div className="dialog__title">
+          {icon}
+          {title}
+        </div>
         <div className="dialog__content">{children}</div>
       </div>
     </div>
