d31d757fe5091149e4e4de8d0b8c7799186581d3
성진옥 <wlsdhr0831@naver.com>
2022-12-08 03:19:35 +0900

    feat: 스토리북에 context 적용

--- src/common/configs/storybook/withWrappers.tsx
+++ src/common/configs/storybook/withWrappers.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { QueryClientProvider } from "react-query";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { Context } from "../../../store";
 import { getGlobalQueryClient } from "../query";
 
 type Props = {
@@ -12,14 +13,16 @@ const withWrappers =
   ({ locationPath, routePath }: Props) =>
   (Story: () => React.ReactElement) => {
     return (
-      <QueryClientProvider client={getGlobalQueryClient()}>
-        <MemoryRouter initialEntries={[locationPath]}>
-          <Routes>
-            <Route path={routePath} element={<Story />} />
-            <Route path="*" element={<Story />} />
-          </Routes>
-        </MemoryRouter>
-      </QueryClientProvider>
+      <Context>
+        <QueryClientProvider client={getGlobalQueryClient()}>
+          <MemoryRouter initialEntries={[locationPath]}>
+            <Routes>
+              <Route path={routePath} element={<Story />} />
+              <Route path="*" element={<Story />} />
+            </Routes>
+          </MemoryRouter>
+        </QueryClientProvider>
+      </Context>
     );
   };
 
