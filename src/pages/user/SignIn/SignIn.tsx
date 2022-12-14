b5c6a9ededbeab6114ab9f0accad6e8c57736a5d
성진옥 <wlsdhr0831@naver.com>
2022-12-15 02:56:38 +0900

    feat: 로그인 후 커뮤니티 홈으로 이동

--- src/pages/user/SignIn/SignIn.tsx
+++ src/pages/user/SignIn/SignIn.tsx
@@ -38,9 +38,7 @@ const SignIn = () => {
             profile: profileUri,
           },
         });
-        // TODO: 전체 게시물 탭으로 보내는지 확인
-        // navigation(BuildPaths.communityHome("0"));
-        navigation(BuildPaths.mypage("RECORD"));
+        navigation(BuildPaths.communityHome("전체 게시물"));
       })
       .catch((error) => {
         alert(error.message);
