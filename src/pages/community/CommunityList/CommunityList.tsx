6fa4d930f080abd3cc426b1df475ccc0a32473cc
성진옥 <wlsdhr0831@naver.com>
2022-12-15 02:49:45 +0900

    feat: 커뮤니티 리스트 hook 추가

--- src/pages/community/CommunityList/CommunityList.tsx
+++ src/pages/community/CommunityList/CommunityList.tsx
@@ -1,96 +1,66 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import BuildPaths from "../../../common/paths";
-import useCategoryListQuery from "../../../common/queries/category/useCategoryListQuery";
-import usePostListByCategoryQuery from "../../../common/queries/posts/usePostListQuery";
 import CommunityListView from "./CommunityList.view";
+import useCommunityList from "./hooks/useCommunityList";
 
 interface SortOrderType {
   type: "CREATED_AT" | "LIKE_COUNT" | "COMMENT_COUNT";
   label: string;
 }
 
+const sortList: SortOrderType[] = [
+  {
+    type: "CREATED_AT",
+    label: "최신순",
+  },
+  {
+    type: "LIKE_COUNT",
+    label: "인기순",
+  },
+  {
+    type: "COMMENT_COUNT",
+    label: "댓글순",
+  },
+];
+
 const CommunityList = () => {
-  const filterList: SortOrderType[] = [
-    {
-      type: "CREATED_AT",
-      label: "최신순",
-    },
-    {
-      type: "LIKE_COUNT",
-      label: "인기순",
-    },
-    {
-      type: "COMMENT_COUNT",
-      label: "댓글순",
-    },
-  ];
-
-  const [sortOrderType, setSortOrderType] = useState<SortOrderType>(
-    filterList[0],
-  );
-
-  const { categoryList } = useCategoryListQuery({
-    categoryGroup: "COMMUNITY",
-  });
-
-  const [activeCategory, setActiveCategory] = useState<{
-    id: number;
-    name: string;
-  }>(
-    categoryList && categoryList.length > 0
-      ? categoryList[0]
-      : { id: 0, name: "전체 게시물" },
-  );
-
-  const { isLoading, postList } = usePostListByCategoryQuery({
-    size: 10,
-    page: 0,
-    sortOrderType: sortOrderType.type,
-    categoryId: activeCategory == null ? undefined : activeCategory.id,
-  });
-
   const navigation = useNavigate();
+  const { category } = useParams<{ category: string }>();
+  const [sortType, setSortType] = useState<SortOrderType>(sortList[0]);
+
+  if (!category) {
+    return <></>;
+  }
+
+  const { isLoading, categoryList, activeCategoryName, communityList } =
+    useCommunityList({
+      category,
+      sortType: sortType.type,
+    });
+
+  if (isLoading) {
+    return <></>;
+  }
 
   return (
     <CommunityListView
       isLoading={isLoading}
-      activeFilter={sortOrderType.label}
-      filterList={filterList.map(filter => filter.label)}
-      activeTab={activeCategory?.name}
-      tabList={
-        categoryList && categoryList.length > 0
-          ? categoryList.map(category => category.name)
-          : ["전체 게시물"]
-      }
-      communityList={
-        postList
-          ? postList!.map(post => {
-              return {
-                id: post.id,
-                user: post.nickname,
-                userImageSrc: post.profileUri,
-                title: post.title,
-                description: post.content,
-                likeCount: post.likeCount,
-                commentCount: post.commentCount,
-                liked: post.likeFlag,
-              };
-            })
-          : []
-      }
-      isEmpty={!(postList && postList.length > 0)}
-      onChangeFilter={(newFilterLabel: string) => {
-        let newFilter: SortOrderType = filterList.filter(
-          filter => filter.label === newFilterLabel,
-        )[0];
-        setSortOrderType(newFilter);
+      activeSort={sortType.label}
+      sortList={sortList.map((item) => item.label)}
+      activeCategory={activeCategoryName}
+      categoryList={categoryList.map((item) => item.name)}
+      communityList={communityList}
+      isEmpty={communityList.length === 0}
+      onChangeSortType={(selectedSort: string) => {
+        setSortType(sortList.filter((item) => item.label === selectedSort)[0]);
       }}
-      onClickTab={(tab: string) => {
-        let newCategory = categoryList!.filter(
-          category => category.name === tab,
-        )[0];
-        setActiveCategory(newCategory);
+      onChangeCategory={(tab: string) => {
+        navigation(
+          BuildPaths.communityHome(
+            categoryList.filter((item) => item.name === tab)[0].name
+          )
+        );
       }}
       onClickItem={(postId: number) => {
         navigation(BuildPaths.communityDetail(postId.toString()));
