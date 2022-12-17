e3d23ba4040424a32ae578a268f65c0646dd72ec
Ho2eny <heoin122@gmail.com>
2022-12-18 03:30:43 +0900

    AudioPlayer 수정

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,28 +1,53 @@
 import { useRef } from "react";
-import { PlayIcon } from "../../assets/icon";
+import { GreyDeleteCircle, PlayIcon } from "../../assets/icon";
 import useOpen from "../../hooks/useOpen";
 import styles from "./AudioPlayer.module.scss";
 
 interface Props {
-  title: string;
+  title?: string;
   src: string;
+  canDelete?: boolean;
+  onClickDelete?: () => void;
 }
 
-const AudioPlayer = ({ title, src }: Props) => {
+const AudioPlayer = ({
+  title,
+  src,
+  canDelete = false,
+  onClickDelete,
+}: Props) => {
   const playerRef = useRef<HTMLAudioElement>(null);
   const { isOpen: isPlaying, onOpen: onPlay, onClose: onPause } = useOpen();
 
   return (
     <div className={styles.container}>
-      {/* <audio src={src} ref={playerRef} /> */}
-      <div className={styles.title}>{title}</div>
+      <audio controls src={src} ref={playerRef} className={styles.audio} />
+      {canDelete && (
+        <GreyDeleteCircle
+          className={styles.delete}
+          onClick={() => onClickDelete?.()}
+        />
+      )}
+
+      {/* <div className={styles.title}>{title}</div>
       <div className={styles.player}>
         {isPlaying ? (
-          <div onClick={onPause}>일시정지</div>
+          <div
+            onClick={() => {
+              playerRef.current?.pause();
+              onPause();
+            }}>
+            일시정지
+          </div>
         ) : (
-          <PlayIcon onClick={onPlay} />
+          <PlayIcon
+            onClick={() => {
+              onPlay();
+              playerRef.current?.play();
+            }}
+          />
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
