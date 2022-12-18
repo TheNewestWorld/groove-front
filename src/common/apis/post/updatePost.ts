e5b685379ef59bb81149ad16fbe5cf5cf469b236
성진옥 <wlsdhr0831@naver.com>
2022-12-18 18:51:34 +0900

    feat: 수정하기 API 수정

--- src/common/apis/post/updatePost.ts
+++ src/common/apis/post/updatePost.ts
@@ -10,18 +10,28 @@ export type UpdatePostBody = {
   title: string;
   content: string;
   categoryId: number;
-  attachments: string[]; // multipart 처리
+  attachments: File[];
 };
 
-export const UpdatePost = (
+export const updatePost = (
   { postId }: UpdatePostParams,
-  body: UpdatePostBody,
+  { title, content, categoryId, attachments }: UpdatePostBody
 ) => {
+  const frm = new FormData();
+  const json = {
+    title,
+    content,
+    categoryId,
+  };
+  const blob = new Blob([JSON.stringify(json)], { type: "application/json" });
+  frm.append("request", blob);
+  attachments.map((file) => frm.append("attachments", file));
+
   return resultData<null>(
-    axios.put(apiUrls.post.updatePost(postId), body, {
+    axios.put(apiUrls.post.updatePost(postId), frm, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
-    }),
+    })
   );
 };
