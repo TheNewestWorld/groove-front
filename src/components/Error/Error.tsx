292955cba2ec20404106caa0c89aacbf67f6db7e
Ho2eny <heoin122@gmail.com>
2022-12-21 03:56:36 +0900

    불필요한 변수 제거

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -6,10 +6,11 @@ const Error = () => {
   const navigation = useNavigate();
 
   useEffect(() => {
-    let timer = setTimeout(() => {
+    setTimeout(() => {
       navigation(-1);
     }, 5000);
   });
+
   return <div className={styles.container}>에러가 발생했습니다...</div>;
 };
 
