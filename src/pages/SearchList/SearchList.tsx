68414598a5a78261377fe56a4535d90623748641
성진옥 <wlsdhr0831@naver.com>
2022-12-21 07:59:39 +0900

    feat: 최근 검색 기록 로직 추가

--- src/pages/SearchList/SearchList.tsx
+++ src/pages/SearchList/SearchList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BuildPaths from "../../common/paths";
 import usePostListByCategoryQuery from "../../common/queries/posts/usePostListQuery";
@@ -6,10 +6,25 @@ import Error from "../../components/Error";
 import Loading from "../../components/Loading";
 import SearchListView from "./SearchList.view";
 
+const spliter = "|";
+const RECENT_SEARCH_KEYWORD = "recentSearchKeyword";
+
+const setLocalStorage = (keywords: string[]) => {
+  keywords = keywords.filter((keyword) => keyword.length > 0);
+
+  if (keywords.length === 0) {
+    localStorage.removeItem(RECENT_SEARCH_KEYWORD);
+  } else {
+    localStorage.setItem(RECENT_SEARCH_KEYWORD, keywords.join(spliter));
+  }
+};
 const SearchList = () => {
   const navigation = useNavigate();
   const [searchWord, setSearchWord] = useState<string>("");
   const [submitKeyword, setSubmitKeyword] = useState<string>("");
+  const [recentSearchKeyword, setRecentSearchKeyword] = useState<string[]>(
+    localStorage.getItem("recentSearchKeyword")?.split(spliter) ?? []
+  );
 
   const { isLoading, isError, postList } = usePostListByCategoryQuery(
     {
@@ -20,58 +35,74 @@ const SearchList = () => {
     },
     {
       enabled: submitKeyword.length !== 0,
-    },
+    }
   );
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  useEffect(() => {
+    setLocalStorage(recentSearchKeyword);
+  }, [recentSearchKeyword]);
 
   if (isError) {
     return <Error />;
   }
 
   return (
-    <SearchListView
-      keyword={searchWord}
-      setKeyword={setSearchWord}
-      communityList={
-        postList?.map(
-          ({
-            id,
-            title,
-            content,
-            nickName,
-            profileUri,
-            likeCount,
-            likeFlag,
-            commentCount,
-          }) => {
-            return {
+    <>
+      {isLoading && <Loading />}
+      <SearchListView
+        keyword={searchWord}
+        setKeyword={setSearchWord}
+        communityList={
+          postList?.map(
+            ({
               id,
-              user: nickName,
-              userImageSrc: profileUri,
               title,
-              description: content,
+              content,
+              nickName,
+              profileUri,
               likeCount,
+              likeFlag,
               commentCount,
-              liked: likeFlag,
-              onClick: () => {
-                navigation(BuildPaths.communityDetail(id.toString()));
-              },
-            };
-          }
-        ) ?? []
-      }
-      onClickCancle={() => navigation(BuildPaths.communityHome("전체 게시물"))}
-      onSubmitSearch={(keyword) => {
-        setSubmitKeyword(keyword);
-      }}
-      deleteItem={() => alert("TODO")}
-      // TODO: 검색 기록 기능 추가
-      list={[]}
-      searchWord={searchWord}
-    />
+            }) => {
+              return {
+                id,
+                user: nickName,
+                userImageSrc: profileUri,
+                title,
+                description: content,
+                likeCount,
+                commentCount,
+                liked: likeFlag,
+                onClick: () => {
+                  navigation(BuildPaths.communityDetail(id.toString()));
+                },
+              };
+            }
+          ) ?? []
+        }
+        onClickCancle={() =>
+          navigation(BuildPaths.communityHome("전체 게시물"))
+        }
+        onSubmitSearch={(keyword) => {
+          setSubmitKeyword(keyword);
+          const newKeywords = [...recentSearchKeyword, keyword];
+          setRecentSearchKeyword(newKeywords);
+        }}
+        deleteItem={(keyword, index) => {
+          const newKeywords = recentSearchKeyword.filter(
+            (word, idx) => keyword !== word || index !== idx
+          );
+          setRecentSearchKeyword(newKeywords);
+        }}
+        list={[
+          {
+            title: "최근 검색 기록",
+            type: "line",
+            itemList: recentSearchKeyword,
+          },
+        ]}
+      />
+    </>
   );
 };
 
