4ac1e661efca6f5225d0318957a4b84e995aa1ac
성진옥 <wlsdhr0831@naver.com>
2022-12-21 02:53:00 +0900

    feat: 코드 정리

--- src/pages/community/CommunityCommentList/CommunityCommentList.tsx
+++ src/pages/community/CommunityCommentList/CommunityCommentList.tsx
@@ -1,21 +1,16 @@
-import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { CloseIcon } from "../../../assets/icon";
 import { deleteComment, postComment } from "../../../common/apis/comment";
 import Error from "../../../components/Error";
 import Header from "../../../components/Header";
 import Loading from "../../../components/Loading";
+import { ReasonType } from "../../../components/ReportBottomSheet/ReportBottomSheet";
 import CommunityCommentListView from "./CommunityCommentList.view";
 import useCommunityCommentList from "./hooks/useCommunityCommentList";
 
 const CommunityCommentList = () => {
   const navigation = useNavigate();
   const { communityId } = useParams<{ communityId: string }>();
-  const [optionStatus, setOptionStatus] =
-    useState<{
-      commentId: number;
-      canEdit: boolean;
-    } | null>(null);
 
   const { isLoading, isError, comments, refetch } = useCommunityCommentList({
     communityId: Number(communityId),
@@ -38,35 +33,25 @@ const CommunityCommentList = () => {
       />
       <CommunityCommentListView
         comments={comments}
-        optionStatus={optionStatus}
         onSubmitComment={(comment: string, parentId?: number) => {
           postComment(
             { postId: Number(communityId) },
             { content: comment, parentId: parentId ?? 0 }
           ).then(() => refetch());
         }}
-        onClickUpdateOption={(commentId: number) => {
+        onClickUpdateComment={(commentId: number) => {
           // TODO(in.heo): 수정 쿼리
           // 수정하기, 답글 달기 내용 구분짓게
           // updateComment({commentId}, )
         }}
-        onClickDeleteOption={(commentId: number) => {
+        onClickDeleteComment={(commentId: number) => {
           deleteComment({ commentId });
         }}
-        onClickReportOption={(commentId: number) => {
+        onClickReport={(commentId: number, value: ReasonType) => {
           // TODO(in.heo): 신고 기능 쿼리
         }}
         onClickReply={(commentId: number) => {
           // TODO(in.heo) 입력 포커싱
-        }}
-        onClickOption={(commentId: number, hasAuthority: boolean) => {
-          setOptionStatus({
-            commentId,
-            canEdit: hasAuthority,
-          });
-        }}
-        onCloseOption={() => {
-          setOptionStatus(null);
         }}
       />
     </>
