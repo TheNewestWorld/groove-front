7e8f8643137c17bc2308474a9eac6c7b8ddfc749
성진옥 <wlsdhr0831@naver.com>
2022-12-21 06:18:23 +0900

    feat: 유저 정보 수정페이지도 서버 정보로 보여주기

--- src/pages/user/EditUserProfile/EditUserProfile.tsx
+++ src/pages/user/EditUserProfile/EditUserProfile.tsx
@@ -6,30 +6,41 @@ import {
   setUserInfo,
   setUserProfileImage,
 } from "../../../common/apis/users";
+import useUserInfoQuery from "../../../common/queries/users/useUserInfoQuery";
 import Header from "../../../components/Header";
 import Loading from "../../../components/Loading";
-import { useUserState, useUserDispatch } from "../../../hooks";
+import Error from "../../../components/Error";
+import { useUserDispatch } from "../../../hooks";
 import EditUserProfileView from "./EditUserProfile.view";
 
 const EditUserProfile = () => {
-  const { name, profile } = useUserState();
+  const { isLoading, isError, data: userInfo } = useUserInfoQuery();
+
   const dispatch = useUserDispatch();
   const navigation = useNavigate();
-  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isUpdating, setUpdating] = useState<boolean>(false);
+
+  if (isLoading || !userInfo) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
-      {isLoading && <Loading />}
+      {isUpdating && <Loading />}
       <Header
         title="프로필 수정"
         left={<ArrowIcon />}
         onClickLeft={() => navigation(-1)}
       />
       <EditUserProfileView
-        src={profile}
-        nickname={name}
+        src={userInfo.profileUri}
+        nickname={userInfo.nickname}
         onClickConfirm={async (newNickname, image) => {
-          setLoading(true);
+          setUpdating(true);
 
           await setUserInfo({ nickname: newNickname });
           await setUserProfileImage({ profile: image });
@@ -45,7 +56,7 @@ const EditUserProfile = () => {
           });
 
           navigation(-1);
-          setLoading(false);
+          setUpdating(false);
         }}
       />
     </>
