2d8adc93742af4ac687aeec65f38a86d1f988f21
gnlwhd6498 <gkgkgldy@gmail.com>
2022-12-21 00:34:35 +0900

    feat: add loading gif

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,15 @@
 import styles from "./Loading.module.scss";
+import gif from "../../assets/image/loading.gif";
 
 const Loading = () => {
-  return <div className={styles.container}>잠시만 기다려주세요.</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img className={styles.gif} alt="gif" src={gif} />
+        <p>잠시만 기다려주세요.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
