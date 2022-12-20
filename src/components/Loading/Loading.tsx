a571473fc952b2eb7d06c8e3d1cefc6ce588bee6
성진옥 <wlsdhr0831@naver.com>
2022-12-21 05:58:56 +0900

    feat: 로딩 컴포넌트 내 이미지 추가

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,16 @@
+import { LoadingImage } from "./images";
 import styles from "./Loading.module.scss";
 
 const Loading = () => {
-  return <div className={styles.container}>해당 데이터를 불러오고 있어요...</div>;
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.image}
+        src={LoadingImage}
+        alt="해당 데이터를 불러오고 있어요..."
+      />
+    </div>
+  );
 };
 
 export default Loading;
---
+++ src/components/Loading/images/index.tsx
@@ -0,0 +1,1 @@
+export { default as LoadingImage } from "./loadingImage.gif";
