7b85baede102b266d27d3c655fbc29c995829db7
Ho2eny <heoin122@gmail.com>
2022-12-24 00:34:59 +0900

    PushList 컨테이너 추가

--- src/pages/PushList/PushList.tsx
+++ src/pages/PushList/PushList.tsx
@@ -1,5 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import useNotificationList from "./hooks/useNotificationList";
+import Header from "../../components/Header";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
+import PushListView from "./PushList.view";
+import { ArrowTailIcon } from "../../assets/icon";
+
 const PushList = () => {
-  return <></>;
+  const navigation = useNavigate();
+  const { isLoading, isError, notificationList } = useNotificationList();
+
+  if (isLoading || !notificationList) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <Error />;
+  }
+
+  return (
+    <>
+      <Header
+        title="알림"
+        left={<ArrowTailIcon />}
+        onClickLeft={() => navigation(-1)}
+      />
+      <PushListView
+        monthList={notificationList.monthList}
+        pastList={notificationList.pastList}
+        emptyText="아직 전해드릴 소식이 없어요."
+        onClickItem={(id: number) => {
+          // TODO: Navigation 추가 핊요. (서버에서 내려오는 linkUrl, notificationType 형태 변경 및 합의 필요)
+        }}
+      />
+    </>
+  );
 };
 
 export default PushList;
---
+++ src/pages/PushList/hooks/useNotificationList.tsx
@@ -0,0 +1,61 @@
+import { GetNotificationListResponse } from "../../../common/apis/notification/getNotificationList";
+import useNotificationListQuery from "../../../common/queries/notification/useNotificationListQuery";
+import { convertToElapsedTime, isThisMonth } from "../helpers";
+
+interface Classification {
+  id: number;
+  src: string;
+  content: string;
+  ago: string;
+}
+
+const useNotificationList = () => {
+  const { isLoading, isError, notificationList } = useNotificationListQuery({
+    size: 10,
+    page: 0,
+  });
+
+  return {
+    isLoading,
+    isError,
+    notificationList: notificationList?.reduce(
+      (result, notification) => {
+        return isThisMonth(new Date(notification.createdAt))
+          ? {
+              ...result,
+              monthList: [
+                ...result.monthList,
+                {
+                  id: notification.id,
+                  // TODO: 서버에서 userId가 아닌 userImage를 주도록 수정 요청 필요
+                  src: notification.userImage,
+                  content: notification.content,
+                  ago: convertToElapsedTime(new Date(notification.createdAt)),
+                },
+              ],
+              all: [...result.all, notification],
+            }
+          : {
+              ...result,
+              pastList: [
+                ...result.monthList,
+                {
+                  id: notification.id,
+                  src: notification.userImage,
+                  content: notification.content,
+                  ago: convertToElapsedTime(new Date(notification.createdAt)),
+                },
+              ],
+              all: [...result.all, notification],
+            };
+      },
+      {
+        monthList: [] as Classification[],
+        pastList: [] as Classification[],
+        all: [] as GetNotificationListResponse["contents"],
+      },
+    ),
+  };
+};
+
+export default useNotificationList;
